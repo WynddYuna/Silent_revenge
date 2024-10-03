0f2c5b7c876451e4e3dff30e56545a35dc421835
SilsuHakdog <127063342+WynddYuna@users.noreply.github.com>
2024-10-03 16:45:30 +0800

    jump function
    no gravity and limit imtegrTEDF YET

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem; 
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    [Header("Movement")]
 
     public float moveSpeed = 5f;
     float horizontalMovement;
+
+      [Header("Movement")]
+
+      public float jumpPower = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +28,10 @@ public class PlayerMovement : MonoBehaviour
     public void Move (InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+    public void Jump (InputAction.CallbackContext context){
+        if(context.performed){
+            rb.velocity = new Vector2(rb.velocity.x,jumpPower); 
+        }
     }
 }
