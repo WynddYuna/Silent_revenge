786ff5e5c79b4b4edad34f890db924020b6f1689
MrHaaaam <deocaresemman@gmail.com>
2024-10-03 17:52:12 +0800

    naiwan doon sa nauna

---
+++ Assets/TabController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabController : MonoBehaviour
+{
+    public Image[] tabImages;
+    public GameObject[] pages;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActivateTab(0);
+    }
+
+    public void ActivateTab(int tabNO)
+    {
+        for (int i = 0; i < pages.Length; i++)
+        {
+            pages[i].SetActive(false);
+            tabImages[i].color = Color.grey;
+        }
+        pages[tabNO].SetActive(true);
+        tabImages[tabNO].color = Color.white;
+    }
+}
