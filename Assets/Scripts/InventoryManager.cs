36c9b87fe1c076893daa113b1e81c562f75f89e7
SilsuHakdog <127063342+WynddYuna@users.noreply.github.com>
2024-10-22 08:22:17 +0800

    Inventory System Part 1

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -4,39 +4,51 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+    public GameObject InventoryMenu; // Reference to the inventory menu GameObject
+    private bool menuActivated; // Tracks whether the menu is active
 
-    public GameObject InventoryMenu;
-    private bool menuActivated;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Ensure the inventory menu is initially inactive
+        InventoryMenu.SetActive(false);
+        menuActivated = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Inventory")&& menuActivated){
-
-            
-            Time.timeScale= 1;      
-
-            InventoryMenu.SetActive(false);
-            menuActivated=false;
-        }
-        else if(Input.GetButtonDown("Inventory")&& !menuActivated){
-
-            Time.timeScale= 0;
-
-            InventoryMenu.SetActive(true);
-            menuActivated=true;
+        // Check for the "E" key press to toggle the inventory
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E key pressed");
+            ToggleInventory();
         }
     }
-    
-    public void AddItem(string itemName, int quantity,Sprite itemSprite)
 
+    // Method to toggle the inventory menu
+    private void ToggleInventory()
     {
-        Debug.Log("itemName = "+ itemName +"quantity = " + quantity + "itemSprite ="+itemSprite);
+        Debug.Log("ToggleInventory called");
+        if (menuActivated)
+        {
+            // If the menu is active, deactivate it
+            Time.timeScale = 1; // Resume game time
+            InventoryMenu.SetActive(false);
+            menuActivated = false;
+        }
+        else
+        {
+            // If the menu is not active, activate it
+            Time.timeScale = 0; // Pause game time
+            InventoryMenu.SetActive(true);
+            menuActivated = true;
+        }
+    }
 
+    // Method to add an item to the inventory
+    public void AddItem(string itemName, int quantity, Sprite itemSprite)
+    {
+        Debug.Log("itemName = " + itemName + ", quantity = " + quantity + ", itemSprite = " + itemSprite);
     }
 }
